3c6946391432b4c3f811bd30624166de38b3324a
Aidan Gallagher <aidan.gallagher@intl.att.com>
2021-06-26 15:59:34 +0100

    Make the remote debs folder path the same as the local folder path

--- build_and_install.py
+++ build_and_install.py
@@ -14,9 +14,10 @@ parser.add_argument('IP address', help='IP address of remote machine')
 args = vars(parser.parse_args())
 ip_addr = args['IP address']
 
+# Set location to store deb files
 # Can't use a tilde in path name so store debs in /tmp rather than $HOME (https://github.com/fabric/fabric/issues/323)
 project = basename(os.getcwd())
-builddir = "/tmp/buildDir/" + project
+builddir = '/tmp/buildDir/' + project
 
 # Used to enter root password on remote system when prompted to
 sudopass = Responder(
@@ -24,23 +25,23 @@ sudopass = Responder(
     response='vyatta\n',
 )
 
-# Build the deb packages and deposit them to ~/buildDir
-subprocess.run([f"osc-buildpkg -D {builddir}"], shell=True)
+# Build debs and deposit them to the buildDir
+subprocess.run([f'osc-buildpkg -D {builddir}'], shell=True)
 
 # Connect to the remote machine
 with Connection(ip_addr, user='vyatta', connect_kwargs={'password': 'vyatta'}) as c:
 
-    # Make folder on remote to deposit packages
-    c.run('mkdir -p /home/vyatta/debs/')
+    # Make destination folder
+    c.run(f'mkdir -p {builddir}')
 
-    # Copy the deb packages over to the VM
-    # Can't specify directory using fabric https://github.com/fabric/fabric/issues/1998
+    # Copy debs to destination
+    # Can't specify directory using fabric (https://github.com/fabric/fabric/issues/1998) so copy each one individually
     debs = glob.glob(f'{builddir}/*.deb')
     for deb in debs:
-        c.put(deb, '/home/vyatta/debs')
+        c.put(deb, builddir)
 
-    # Install the deb packages
-    c.sudo('dpkg -i /home/vyatta/debs/*.deb', watchers=[sudopass])
+    # Install debs
+    c.sudo(f'dpkg -i {builddir}/*.deb', watchers=[sudopass])
 
     # Restart the VM
     c.sudo('reboot', watchers=[sudopass])
